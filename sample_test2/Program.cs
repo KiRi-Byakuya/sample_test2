6046dee86c3797b4113f86081b30886bcc875e6c
KiRi-Byakuya <gt86.brz.sub@gmail.com>
2015-03-10 20:59:10 +0900

    テスト(新規追加)

---
+++ sample_test2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_test2 {
+    class Program {
+        static void Main(string[] args) {
+
+            int i = 0;
+            byte x = 0xFF;
+
+            Console.Write("テスト\n");
+            Console.Write("i = {0}, x = {1}", i, x);
+
+            System.Console.ReadLine();
+        }
+    }
+}
